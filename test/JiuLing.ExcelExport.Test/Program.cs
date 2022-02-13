f9480113760d1643d3981f8de89c3d7ba26db1aa
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-02-13 14:36:08 +0800

    1、测试程序的控制台输出表格内容

--- test/JiuLing.ExcelExport.Test/Program.cs
+++ test/JiuLing.ExcelExport.Test/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.IO;
+using System.Linq;
 
 namespace JiuLing.ExcelExport.Test
 {
@@ -15,11 +16,28 @@ namespace JiuLing.ExcelExport.Test
                 ds.Tables.Add(GetTable2());
                 ds.Tables.Add(GetTableTitle());
 
+                string s = DataTableToString(ds.Tables[0]);
+                Console.WriteLine(ds.Tables[0].TableName);
+                Console.WriteLine(s);
+                Console.WriteLine();
+
+                s = DataTableToString(ds.Tables[1]);
+                Console.WriteLine(ds.Tables[1].TableName);
+                Console.WriteLine(s);
+                Console.WriteLine();
+
+                s = DataTableToString(ds.Tables[2]);
+                Console.WriteLine(ds.Tables[2].TableName);
+                Console.WriteLine(s);
+                Console.WriteLine();
+
                 var templateFile = Path.Combine(System.AppContext.BaseDirectory, "Template.xlsx");
                 var destinationFile = Path.Combine(System.AppContext.BaseDirectory, "test.xlsx");
 
                 new TemplateData().Export(templateFile, destinationFile, ds);
                 Console.WriteLine("导出完成");
+                Console.WriteLine($"模板文件：{templateFile}");
+                Console.WriteLine($"导出文件：{destinationFile}");
             }
             catch (Exception ex)
             {
@@ -52,7 +70,7 @@ namespace JiuLing.ExcelExport.Test
 
             dr = dt.NewRow();
             dr["Class"] = "2班";
-            dr["Name"] = "刘星";
+            dr["Name"] = "王五";
             dr["Score"] = "72";
             dt.Rows.Add(dr);
             return dt;
@@ -91,7 +109,14 @@ namespace JiuLing.ExcelExport.Test
             dr["Name"] = "课程表";
             dr["Time"] = DateTime.Now;
             dt.Rows.Add(dr);
+
             return dt;
+        }
+
+
+        private static string DataTableToString(DataTable dt)
+        {
+            return string.Join(Environment.NewLine, dt.Rows.OfType<DataRow>().Select(x => string.Join(" ; ", x.ItemArray)));
         }
     }
 }
