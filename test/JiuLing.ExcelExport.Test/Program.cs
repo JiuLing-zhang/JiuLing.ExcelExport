bf6ec7be41024a7f7ad8c982d58108140c467aa1
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-02-13 14:42:46 +0800

    1、测试数据加入多Sheet页

--- test/JiuLing.ExcelExport.Test/Program.cs
+++ test/JiuLing.ExcelExport.Test/Program.cs
@@ -15,6 +15,10 @@ namespace JiuLing.ExcelExport.Test
                 ds.Tables.Add(GetTable1());
                 ds.Tables.Add(GetTable2());
                 ds.Tables.Add(GetTableTitle());
+                ds.Tables.Add(GetTable3());
+
+                Console.WriteLine("测试数据源：");
+                Console.WriteLine();
 
                 string s = DataTableToString(ds.Tables[0]);
                 Console.WriteLine(ds.Tables[0].TableName);
@@ -28,6 +32,12 @@ namespace JiuLing.ExcelExport.Test
 
                 s = DataTableToString(ds.Tables[2]);
                 Console.WriteLine(ds.Tables[2].TableName);
+                Console.WriteLine(s);
+                Console.WriteLine();
+
+
+                s = DataTableToString(ds.Tables[3]);
+                Console.WriteLine(ds.Tables[3].TableName);
                 Console.WriteLine(s);
                 Console.WriteLine();
 
@@ -45,7 +55,9 @@ namespace JiuLing.ExcelExport.Test
             }
             finally
             {
-                Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("按任意键退出....");
+                Console.ReadKey();
             }
         }
 
@@ -113,6 +125,32 @@ namespace JiuLing.ExcelExport.Test
             return dt;
         }
 
+        private static DataTable GetTable3()
+        {
+            var dt = new DataTable("dt3");
+            dt.Columns.Add("Class");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Score");
+
+            var dr = dt.NewRow();
+            dr["Class"] = "9班";
+            dr["Name"] = "小张";
+            dr["Score"] = "100";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["Class"] = "9班";
+            dr["Name"] = "小王";
+            dr["Score"] = "99";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["Class"] = "8班";
+            dr["Name"] = "小李";
+            dr["Score"] = "98";
+            dt.Rows.Add(dr);
+            return dt;
+        }
 
         private static string DataTableToString(DataTable dt)
         {
